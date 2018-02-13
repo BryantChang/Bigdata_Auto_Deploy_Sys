2079474c0abef5e243f5e4ebb610c866f80d4df8
BryantChang <finish740@sina.com>
2018-02-13 18:34:01 +0800

    add api of add cluster

--- web/src/main/webapp/static/js/autosys/clustermanage/clusterops.js
+++ web/src/main/webapp/static/js/autosys/clustermanage/clusterops.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#submitAddCluster').click(function(){
         var clustername = $('#clustername').val();
         var cdesc = $('#cdesc').val();
-        var infoid = $('#infoid').prop("selected");
+        var infoid = $('#infoid').val();
         alert(infoid)
     });
 });
